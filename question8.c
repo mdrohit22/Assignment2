321653d82301d41030fb05734c61cb9f560ca394
Md Rohit <108532993+mdrohit22@users.noreply.github.com>
2022-07-20 20:40:24 +0530

    Add files via upload

---
+++ question8.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+
+    int num;
+    printf("Enter a number\n");
+    scanf("%d", &num);
+    if((num&1)==0)
+       printf("%d is a even number", num);
+    else 
+       printf("%d is a odd number", num);
+
+
+return 0;
+}
