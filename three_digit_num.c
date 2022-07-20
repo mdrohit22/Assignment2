8b5a59c0a379a1a016bc96a1fa06abd5c7715fda
Md Rohit <108532993+mdrohit22@users.noreply.github.com>
2022-07-20 18:48:31 +0530

    Add files via upload

---
+++ three_digit_num.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+
+    int a, b, c , sum= 0;
+    printf("Enter 3 values\n");
+    scanf("%d %d %d", &a, &b, &c);
+    sum  = a + b +c;
+    printf("The value of sum is %d", sum);
+
+
+
+
+return 0;
+}
