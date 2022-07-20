22bbd429ae4c3ac290193009cbc5aebbbc797948
Md Rohit <108532993+mdrohit22@users.noreply.github.com>
2022-07-20 19:14:22 +0530

    Add files via upload

---
+++ question6.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    char a;
+    printf("Enter any character on you keybord\n");
+    scanf("%c", &a);
+    printf("ASCII code of %c is %d",a,a);
+    
+    
+
+
+
+return 0;
+}
